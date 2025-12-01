016e60cb9f71e9513d59e5d5a6a17616842607cd
feliperandon <feliperandon32@gmail.com>
2025-12-01 15:19:27 -0300

    app first commit

---
+++ estoque/src/App.tsx
@@ -0,0 +1,10 @@
+import Button from "@/components/Button";
+
+export const App = () => {
+  return (
+    <div>
+      <h1 className="text-red-500">Hello World</h1>
+      <Button />
+    </div>
+  );
+};
