63cb9da6f5aafeb84059cd9f9b2cc29737ca5743
feliperandon <112654593+feliperandon@users.noreply.github.com>
2025-12-07 13:36:43 -0300

    feature: connected products page to products store

--- estoque/src/pages/Products.tsx
+++ estoque/src/pages/Products.tsx
@@ -1,15 +1,10 @@
 import Header from "@/components/layout/Header";
 import ProductCard from "@/features/products/components/ProductCard";
-
-const products = [
-  { id: "1", name: "Camiseta Azul", quantity: 10 },
-  { id: "2", name: "Pulseira Rosa", quantity: 5 },
-  { id: "4", name: "Bolsa Preta", quantity: 3 },
-  { id: "5", name: "Bolsa Preta", quantity: 3 },
-  { id: "6", name: "Bolsa Preta", quantity: 3 },
-];
+import { useProductsStore } from "@/features/products/hooks/useProductsStore";
 
 const Products = () => {
+  const products = useProductsStore((state) => state.products);
+
   return (
     <div>
       <Header title="Produtos" description="Gerencie seus produtos." />
