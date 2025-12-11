04c6f29fab588e689c7f772c01d201875cedd7de
feliperandon <feliperandon32@gmail.com>
2025-12-11 12:08:57 -0300

    fix: header padding

--- estoque/src/components/layout/Header.tsx
+++ estoque/src/components/layout/Header.tsx
@@ -8,7 +8,7 @@ type HeaderProps = {
 
 const Header = ({ title, description, className }: HeaderProps) => {
   return (
-    <div className={cn("flex flex-col mb-6 py-6 gap-1", className)}>
+    <div className={cn("flex flex-col mb-6 gap-1", className)}>
       <h1 className="text-3xl font-semibold text-white">{title}</h1>
       <p className="text-white/70 text-sm">{description}</p>
     </div>
--- estoque/src/components/layout/MainLayout.tsx
+++ estoque/src/components/layout/MainLayout.tsx
@@ -6,7 +6,7 @@ const MainLayout = () => {
   return (
     <div className="flex h-screen">
       <Sidebar />
-      <div className="flex-1 overflow-auto p-8 bg-[#474747]">
+      <div className="flex-1 overflow-auto px-8 py-4 bg-[#474747]">
         <main className="flex-1 overflow-auto bg-gray-50">
           <Outlet />
         </main>
